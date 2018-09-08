45f85e596fa987a2dbe720c6490a37a7c564a796
Samantha Hyatt <shhyatt@gmail.com>
2018-09-08 22:26:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
 function theBeatlesPlay (musicians, intruments) {
   var newArray = [];
-  for (var i = 0; i < musicians.length)
+  for (var i = 0; i < musicians.length; i++);
 
