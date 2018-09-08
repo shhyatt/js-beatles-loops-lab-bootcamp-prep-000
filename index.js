2e723a4e4c8781495943f380088b7e29478e811e
Samantha Hyatt <shhyatt@gmail.com>
2018-09-08 22:40:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ function theBeatlesPlay (musicians, intruments) {
 var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
 
 function johnLennonFacts(array) {
+  var result = [];
+  
   
   
   
