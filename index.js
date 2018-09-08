95dd45cfdc26b3bc439a66bd4992ec0740d50699
Samantha Hyatt <shhyatt@gmail.com>
2018-09-08 22:28:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,7 @@ function theBeatlesPlay (musicians, intruments) {
   for (var i = 0; i < musicians.length; i++) {
     newArray.push(musicians[i] + "plays" + instruments[i]);
   }
-  
+  return newArray;
+}
+
 
